a415c4e744be9e964b91f803fb404b676ee12e92
Ganzha Eduard <edik.ganzha.05@gmail.com>
2024-07-08 16:06:22 +0900

    Fix parsing

--- fefu_data.py
+++ fefu_data.py
@@ -9,17 +9,18 @@ import pickle
 """ Сбор данных """
 
 
-def get_speciality(url, finance):
+def get_speciality(url):
     """ Напишем функцию для получения списка специальностей c источником финансирования
-                         - finance ('Бюджетная основа', 'Полное возмещение затрат').
+                         - finance 'Бюджет'.
     """
 
     user_agent = {
         'user-agent': 'Mozilla/5.0 (X11; Linux aarch64) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/88.0.4324.188 Safari/537.36 CrKey/1.54.250320'}
+
     data = {
         'admissionCampaignType': "Прием на обучение на бакалавриат/специалитет",
-        'financingSource': finance,
+        'financingSource' : 'Бюджет',
         'studyForm': "Очная"
     }
 
@@ -27,14 +28,14 @@ def get_speciality(url, finance):
     return raw['data']
 
 
-def get_data(url, department, finance, data_students, count_place, orig):
+def get_data(url, department, data_students, count_place, orig):
     """Сбор данных о поступающих абитуриентах на определенное направление - department. """
 
     # Словарь ключей для получения кол-во мест
-    place = {'Имеющие особое право': 'SpecialQuotaCount',
-             'На общих основаниях': 'BudgetQuotaCount',
+    place = {'Особая квота': 'SpecialQuotaCount',
+             'Основные места': 'BudgetQuotaCount',
              'Отдельная квота': 'SeparateQuotaCount',
-             'Целевой прием': 'TargetQuotaCount',
+             'Целевая квота': 'TargetQuotaCount',
              'Полное возмещение затрат': 'ExtraBudgetQuotaCount'}
 
     user_agent = {
@@ -42,7 +43,7 @@ def get_data(url, department, finance, data_students, count_place, orig):
                       'Chrome/88.0.4324.188 Safari/537.36 CrKey/1.54.250320'}
     data = {
         'admissionCampaignType': "Прием на обучение на бакалавриат/специалитет",
-        'financingSource': finance,
+        'financingSource': 'Бюджет',
         'studyForm': "Очная",
         'trainingDirection': department,
         'sortDirection': "sum",
@@ -79,20 +80,17 @@ def get_data(url, department, finance, data_students, count_place, orig):
     return data_students, count_place
 
 
-def data_loading(finance, data_students, count_place, orig):
+def data_loading(data_students, count_place, orig):
     """ Загрузка данных с сайта """
 
-    url_data = 'https://www.dvfu.ru/bitrix/services/main/' \
-               'ajax.php?mode=class&c=dvfu%3Aadmission.spd.new&action=getStudents'
-    url_speciality = "https://www.dvfu.ru/bitrix/services/main/" \
-                     "ajax.php?mode=class&c=dvfu%3Aadmission.spd.new&action=getTrainingDirectionList"
+    url_data =  "https://www.dvfu.ru/bitrix/services/main/ajax.php?admissionCampaignType=%D0%9F%D1%80%D0%B8%D0%B5%D0%BC%20%D0%BD%D0%B0%20%D0%BE%D0%B1%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5%20%D0%BD%D0%B0%20%D0%B1%D0%B0%D0%BA%D0%B0%D0%BB%D0%B0%D0%B2%D1%80%D0%B8%D0%B0%D1%82%2F%D1%81%D0%BF%D0%B5%D1%86%D0%B8%D0%B0%D0%BB%D0%B8%D1%82%D0%B5%D1%82&financingSource=%D0%91%D1%8E%D0%B4%D0%B6%D0%B5%D1%82&studyForm=%D0%9E%D1%87%D0%BD%D0%B0%D1%8F&trainingDirection=01.03.02%20%D0%9F%D1%80%D0%B8%D0%BA%D0%BB%D0%B0%D0%B4%D0%BD%D0%B0%D1%8F%20%D0%BC%D0%B0%D1%82%D0%B5%D0%BC%D0%B0%D1%82%D0%B8%D0%BA%D0%B0%20%D0%B8%20%D0%B8%D0%BD%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%82%D0%B8%D0%BA%D0%B0&sortDirection=sum&enrolled=N&sendorig=N&topprior=N&mode=class&c=dvfu%3Aadmission.spd&action=getStudents"
+    url_speciality = "https://www.dvfu.ru/bitrix/services/main/ajax.php?admissionCampaignType=%D0%9F%D1%80%D0%B8%D0%B5%D0%BC%20%D0%BD%D0%B0%20%D0%BE%D0%B1%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5%20%D0%BD%D0%B0%20%D0%B1%D0%B0%D0%BA%D0%B0%D0%BB%D0%B0%D0%B2%D1%80%D0%B8%D0%B0%D1%82%2F%D1%81%D0%BF%D0%B5%D1%86%D0%B8%D0%B0%D0%BB%D0%B8%D1%82%D0%B5%D1%82&financingSource=%D0%91%D1%8E%D0%B4%D0%B6%D0%B5%D1%82&studyForm=%D0%9E%D1%87%D0%BD%D0%B0%D1%8F&mode=class&c=dvfu%3Aadmission.spd&action=getTrainingDirectionList"
 
     # Загрузка специальностей
-    fefu_speciality = get_speciality(url_speciality, finance)
-
+    fefu_speciality = get_speciality(url_speciality)
     # Загрузка данных об абитуриентах
     for speciality in fefu_speciality:
-        data_students, count_place = get_data(url_data, speciality, finance, data_students, count_place, orig)
+        data_students, count_place = get_data(url_data, speciality, data_students, count_place, orig)
 
     return data_students, count_place
 
@@ -164,78 +162,74 @@ def incoming_students(category, students_received, count_place, data_students):
 
 
 """Для бюджетной основы сначала определяем поступивших абитуриентов на общих основаниях, затем на квотные места, 
-   оставшиеся места переходят в места 'На общих основаниях', дополняем списки 'На общих основаниях'.
-   Для платной основы всё легче, так как присутствуют только места 'На общих основаниях'.
+   оставшиеся места переходят в места 'Основные места', дополняем списки 'Основные места'.
 """
 
 
-def get_students_received(finance, students_received, count_place, data_students):
+def get_students_received(students_received, count_place, data_students):
     """ Формирование DataFrame с поступившими абитуриентами """
 
     # Определяем поступивших абитуриентов на общих основаниях
-    students_received = incoming_students('На общих основаниях', students_received, count_place, data_students)
+    students_received = incoming_students('Основные места', students_received, count_place, data_students)
 
-    if finance == 'Бюджетная основа':
-        kvota = ['Имеющие особое право', 'Отдельная квота', 'Целевой прием']
+    kvota = ['Особая квота', 'Отдельная квота', 'Целевая квота']
 
-        for category in kvota:
-            students_received = incoming_students(category, students_received, count_place, data_students)
+    for category in kvota:
+        students_received = incoming_students(category, students_received, count_place, data_students)
 
-        # Добавляем оставшиеся места в общие основания
-        for category in kvota:
-            for speciality in count_place[category]:
-                count_place['На общих основаниях'][speciality] += count_place[category][speciality]
-                count_place[category][speciality] = 0
+    # Добавляем оставшиеся места в общие основания
+    for category in kvota:
+        for speciality in count_place[category]:
+            count_place['Основные места'][speciality] += count_place[category][speciality]
+            count_place[category][speciality] = 0
 
-        # Дополняем поступивших абитуриентов на общих основаниях
-        students_received = incoming_students('На общих основаниях', students_received, count_place, data_students)
+    # Дополняем поступивших абитуриентов на общих основаниях
+    students_received = incoming_students('Основные места', students_received, count_place, data_students)
 
     return students_received
 
 
 
 if __name__ == "__main__":
-    type_place = {'Бюджетная основа': 'budget',
-                  'Полное возмещение затрат': 'paid'}
+    finance = 'Бюджет'
 
-    # Выберите категорию аттестата: 'N' - не учитывая наличие оригинала, 'Y' - только оригинал документа
     orig = input('Выберите категорию аттестата: "N" - не учитывая наличие оригинала'
                  ', "Y" - только оригинал документа: ')
+    assert orig in "YN", "Категория не корректна!"
 
-    for finance in type_place:
-        # Словарь для хранения информации о кол-во мест
-        count_place = {}
-        # Будущий DataFrame для хранения информации об абитуриентах
-        data_students = {'СНИЛС': [], 'Специальность': [], 'Балл': [], 'Приоритет': [], 'БВИ': [], 'Категория': []}
+    # Словарь для хранения информации о кол-во мест
+    count_place = {}
+    # Будущий DataFrame для хранения информации об абитуриентах
+    data_students = {'СНИЛС': [], 'Специальность': [], 'Балл': [], 'Приоритет': [], 'БВИ': [], 'Категория': []}
 
-        # Загрузим данные
-        data_students, count_place = data_loading(finance, data_students, count_place, orig)
-        # Удалим дубликаты
-        data_students = pd.DataFrame(data_students).drop_duplicates()
+    # Загрузим данные
+    data_students, count_place = data_loading(data_students, count_place, orig)
+    # Удалим дубликаты
+    data_students = pd.DataFrame(data_students).drop_duplicates()
 
-        # Создадим переменную для хранения поступивших абитуриентов
-        students_received = pd.DataFrame({'СНИЛС': [], 'Специальность': [], 'Балл': [],
-                                          'Приоритет': [], 'БВИ': [], 'Категория': []})
+    # Создадим переменную для хранения поступивших абитуриентов
+    students_received = pd.DataFrame({'СНИЛС': [], 'Специальность': [], 'Балл': [],
+                                      'Приоритет': [], 'БВИ': [], 'Категория': []})
 
-        # Сформируем DataFrame с поступившими абитуриентами
-        students_received = get_students_received(finance, students_received, count_place, data_students)
+    # Сформируем DataFrame с поступившими абитуриентами
+    students_received = get_students_received(students_received, count_place, data_students)
 
-        # Формирование DataFrame с проходными баллами
-        passing_score = students_received[students_received['БВИ'] == False] \
-            .groupby(['Специальность', 'Категория'], as_index=False) \
-            .agg({'Балл': min}).sort_values(by='Категория')
+    # Формирование DataFrame с проходными баллами
+    passing_score = students_received[students_received['БВИ'] == False] \
+        .groupby(['Специальность', 'Категория'], as_index=False) \
+        .agg({'Балл': min}).sort_values(by='Категория')
 
-        # Сохраняем кол-во мест
-        with open(f'./count_place_{type_place[finance]}.pkl', 'wb') as f:
-            pickle.dump(count_place, f)
+    # Сохраняем кол-во мест
+    with open(f'./count_place_budget.pkl', 'wb') as f:
+        pickle.dump(count_place, f)
 
-        # Сохраняем поступивших абитуриентов
-        students_received.to_excel(f'./students_received_{type_place[finance]}.xlsx')
+    # Сохраняем поступивших абитуриентов
+    students_received.to_excel(f'./students_received_budget.xlsx')
 
-        # Сохраняем проходные баллы в файл
-        writer = pd.ExcelWriter(f'./passing_score_{type_place[finance]}.xlsx', engine='xlsxwriter')
+    # Сохраняем проходные баллы в файл
+    writer = pd.ExcelWriter(f'./passing_score_budget.xlsx', engine='xlsxwriter')
 
-        for category in passing_score['Категория'].unique():
-            passing_score[passing_score['Категория'] == category].to_excel(writer, sheet_name=category)
+    for category in passing_score['Категория'].unique():
+        passing_score[passing_score['Категория'] == category].to_excel(writer, sheet_name=category)
 
-        writer.close()
+    writer.close()
